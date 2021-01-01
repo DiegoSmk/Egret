94d16a8249cdc661eff8ed38c8e06672a10bdfbf
Diego Pena <diego.nib@outlook.com>
2021-01-01 10:44:55 -0300

    make api timeline

---
+++ src/content/data.js
@@ -0,0 +1,166 @@
+export const timelineData = [
+  {
+    text: 'Site no ar',
+    date: '13 de dezembro de 2020',
+    category: {
+      tag: 'Egret',
+      color: '#9b51e0'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  },
+  {
+    text: 'Wrote my first blog post ever on Medium',
+    date: 'March 03 2017',
+    category: {
+      tag: 'medium',
+      color: '#018f69'
+    },
+    link: {
+      url:
+        'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+      text: 'Read more'
+    }
+  }
+]
